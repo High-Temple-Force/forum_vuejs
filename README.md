7507aa746491b96662cff10184130f3fbaa919aa
bluesDD <dd11blues@gmail.com>
2018-12-15 01:23:55 +0900

    Initialized backend

---
+++ README.md
@@ -0,0 +1,11 @@
+## Installation
+1. npm init
+2. npm i express cors body-parser mongodb
+3. npm i -D nodemon
+4. change package.json as below:
+5. "scripts": {
+    "start": "node server/index.js",
+    "dev": "nodemon server/index.js"
+  },
+
+6. 
---
+++ server/routes/api/posts.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongodb = require('mongodb')
+
+const router = express.Router()
+
+// get posts
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection()
+  res.send(await posts.find({}).toArray())
+})
+
+// add posts
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection()
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  })
+  res.status(201).send()
+})
+
+// delete posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection()
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    res.status(200).send()
+})
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect
+  ('mongodb://moby:5kouraku@ds037488.mlab.com:37488/vue-forum', {
+    useNewUrlParser: true
+  })
+
+  return client.db('vue-forum').collection('posts')
+}
+
+module.exports = router
