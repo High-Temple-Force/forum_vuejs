d9c370bad98e1230ecf735fbdd8b67f799280ebb
bluesDD <dd11blues@gmail.com>
2018-12-15 14:04:46 +0900

    Added static folder

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
@@ -11,6 +12,15 @@ app.use(cors())
 const posts = require('./routes/api/posts')
 
 app.use('/api/posts', posts)
+
+// handle product
+if (process.env.NODE_ENV === 'production') {
+  // static folder
+  app.use(express.static(path.join(__dirname, 'public/')))
+
+  // handle spa
+  app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+}
 
 const port = process.env.PORT || 5000
 
